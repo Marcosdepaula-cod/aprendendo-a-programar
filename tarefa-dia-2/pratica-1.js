b4cdfa95c9340f394296cf320bc0820452760eb8
Marcosdepaula-cod <marquinhochievo83@gmail.com>
2022-08-25 19:59:49 -0300

    atual 1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,8 @@ let email = "marquinhochievo@hotmail.com"
 let profissão = "designer"
 let idade = "38"
 let viagem ="indonesia"
+console.log(nome)
+console.log(email)
+console.log(profissão)
+console.log(idade)
+console.log(viagem)
