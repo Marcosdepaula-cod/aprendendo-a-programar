f3ea059def9754978796f3f87aaaab511fba73a7
Marcosdepaula-cod <marquinhochievo83@gmail.com>
2022-08-30 07:35:56 -0300

    tarefa

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -15,6 +15,12 @@ console.log(profissão)
 console.log(idade)
 console.log(viagem)
 
+let nome = "Marcos"
+let email = "marquinhochievo@hotmail.com"
+let profissão = "designer"
+let idade = "38"
+let viagem = "Indonésia" 
+
 Olá, "Marcos"! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para "Indonésia". Confirma pra gente alguns dados? A sua idade "38" que armazena sua idade>, você é "designer" e o seu email é "marquinhochievo@hotmail.com"? Ficamos no aguardo. Muito obrigado e boa viagem! :D
 
 console.log(nome)
