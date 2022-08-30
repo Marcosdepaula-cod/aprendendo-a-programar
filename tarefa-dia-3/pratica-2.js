b056c743792b8b9b9b9f2d75e24528ddbeed51fd
Marcosdepaula-cod <marquinhochievo83@gmail.com>
2022-08-30 11:08:34 -0300

    tarefa

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,9 @@
+let multiplicador = 1
+let n = 10
+let i = 1
+
+while (i <= n) {let resultado = multiplicador * i
+    let textoDeExibicao = '${multipliccador} x ${i} =${calculoTabuada}'
+console.log (textoDeExibicao)
+1++
+}
