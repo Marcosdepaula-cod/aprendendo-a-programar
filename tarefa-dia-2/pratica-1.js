1feca09b7bd8de73f614762be09653257836597f
Marcosdepaula-cod <marquinhochievo83@gmail.com>
2022-08-30 06:09:27 -0300

    agencia de viagem

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,4 @@ console.log("marquinhochievo@hotmail.com")
 console.log("designer")
 console.log("38")
 console.log("Indonésia")
+Olá, console.log("Marcos")! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para console.log("Indonésia"). Confirma pra gente alguns dados? A sua idade é console.log("38"), você é console.log("designer") e o seu email é console.log("marquinhochievo@hotmail.com")? Ficamos no aguardo. Muito obrigado e boa viagem! :D
